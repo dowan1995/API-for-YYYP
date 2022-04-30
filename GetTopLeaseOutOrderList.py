0ca32cde070daa0bf02a860c0dbb608808aa595c
Dowan <46449568+dowan1995@users.noreply.github.com>
2022-04-30 13:08:10 +0800

    Update GetTopLeaseOutOrderList.py

--- GetTopLeaseOutOrderList.py
+++ GetTopLeaseOutOrderList.py
@@ -1,112 +1,122 @@
 import os
 import json
-import sys
-
 import xlrd
 import xlwt
 import time
 import requests
-import pandas as pd
 from goods import goods
 from xlutils.copy import copy
 
-goods_id = int(sys.argv[1])
 
-url = f"https://api.youpin898.com/api/trade/Order/GetTopLeaseOutOrderList?TemplateId={goods_id}"
-
-
-def request_data():
+def requestApiData(url):
     """
-    GET 获取接口返回的 JSON数据
+    GET 获取接口返回的 JSON 数据
     """
     req = requests.get(url, timeout=30)
     req_json = req.json()
     return req_json
 
 
-def json_file():
+def createJsonFile(goodsid):
     """
     判断文件是否存在，不存在则创建；
     在该文件夹下创建 HTTP GET 返回的 JSON 数据文件
     """
-    if not os.path.exists("excel"):
-        os.mkdir("excel")
-    with open(f"{goods_id}_data.json", "w") as fp:
-        fp.write(json.dumps(request_data()['Data'], sort_keys=True, indent=4, separators=(',', ': ')))
+    with open(f"{goodsid}_data.json", "w") as fp:
+        fp.write(json.dumps(requestApiData(API_URL)['Data'], sort_keys=True, indent=4, separators=(',', ': ')))
 
 
-def main():
-    request_data()
-    json_file()
+def readDataFromJson(goodsid):
+    """
 
-    filename_date_string = time.strftime("%Y%m%d")
-
-    # 创建 Excel 工作表
-    workbook = xlwt.Workbook(encoding='utf-8')
-    worksheet = workbook.add_sheet('sheet1')
-
-    # 设置表头
-    worksheet.write(0, 0, label="租赁价格")
-    worksheet.write(0, 1, label="租赁时长")
-    worksheet.write(0, 2, label="租赁类型")
-    worksheet.write(0, 3, label="押金")
-    worksheet.write(0, 4, label="成交时间")
-    worksheet.write(0, 5, label="数据获取时间")
-
-    # 如果不存在该文件就创建
-    filename = f'excel/{goods[goods_id]}_租赁成交记录_' + filename_date_string + '.xls'
-    if not os.path.exists(filename):
-        workbook.save(filename)
-
-    # 读取 JSON 文件后删除该文件
-    with open(f'{goods_id}_data.json', 'r') as f:
+    :return:
+    """
+    with open(f'{goodsid}_data.json', 'r') as f:
         data = json.load(f)
-        os.remove(f'{goods_id}_data.json')
+        os.remove(f'{goodsid}_data.json')
+    return data
 
-    # pandas 读取 Excel，获取当前行数
-    df = pd.read_excel(filename, engine='xlrd', sheet_name='sheet1')
-    row_num = len(df.index.values) + 1
 
-    # 多次写入，将旧 workbook 拷贝进新的 workbook，并在新的 workbook 基础上 write 数据
-    oldworkbook = xlrd.open_workbook(filename)
-    newworkbook = copy(oldworkbook)
-    newworksheet = newworkbook.get_sheet('sheet1')
+def createExcel(excelfilename):
+    """
+    :return:
+    """
+    # 创建 Excel
+    workbook = xlwt.Workbook(encoding='utf-8')
+    workbook.add_sheet("initialization")
+    workbook.save(excelfilename)
+
+
+def createSheet(oldworkbook, goodsid, excelfilename):
+    if f"{goods[goodsid]}" in oldworkbook.sheet_names():
+        pass
+    else:
+        NewWorkBook = copy(oldworkbook)
+        NewWorkSheet = NewWorkBook.add_sheet(goods[goodsid])
+        NewWorkSheet.write(0, 0, label="租赁价格")
+        NewWorkSheet.write(0, 1, label="租赁时长")
+        NewWorkSheet.write(0, 2, label="租赁类型")
+        NewWorkSheet.write(0, 3, label="押金")
+        NewWorkSheet.write(0, 4, label="成交时间")
+        NewWorkBook.save(excelfilename)
+
+
+def writeDataToSheet(goodsid, excelfilename, data):
+    OldWorkBook = xlrd.open_workbook(excelfilename)
+    NewWorkBook = copy(OldWorkBook)
+    NewWorkSheet = NewWorkBook.get_sheet(goods[goodsid])
+
+    RowCount = xlrd.open_workbook(ExcelFileName).sheet_by_name(goods[GoodsId]).nrows
 
     # JSON数据写入表格
-    val1 = row_num
-    val2 = row_num
-    val3 = row_num
-    val4 = row_num
-    val5 = row_num
-    val6 = row_num
+    val1 = RowCount
+    val2 = RowCount
+    val3 = RowCount
+    val4 = RowCount
+    val5 = RowCount
     for list_item in data:
         for key, value in list_item.items():
-            data_date_string = time.strftime("%Y%m%d-%H%M%S")
             if key == "LeaseUnitPrice":
-                newworksheet.write(val1, 0, value)
+                NewWorkSheet.write(val1, 0, value)
                 val1 += 1
             elif key == "LeaseDays":
-                newworksheet.write(val2, 1, value)
+                NewWorkSheet.write(val2, 1, value)
                 val2 += 1
             elif key == "Type":
                 if value == 1:
                     value = "长租"
                 elif value == 2:
                     value = "短租"
-                newworksheet.write(val3, 2, value)
+                NewWorkSheet.write(val3, 2, value)
                 val3 += 1
             elif key == "LeaseDeposit":
-                newworksheet.write(val4, 3, value)
+                NewWorkSheet.write(val4, 3, value)
                 val4 += 1
             elif key == "DateTime":
-                newworksheet.write(val5, 4, value)
+                NewWorkSheet.write(val5, 4, value)
                 val5 += 1
             else:
                 pass
-        newworksheet.write(val6, 5, data_date_string)
-        val6 += 1
-    newworkbook.save(filename)
+    NewWorkBook.save(excelfilename)
 
 
 if __name__ == '__main__':
-    main()
+    # Excel 文件名
+    ExcelFileName = f'租赁成交记录_' + time.strftime("%Y%m%d") + '.xls'
+
+    for GoodsId in goods:
+        # 接口地址
+        API_URL = f"https://api.youpin898.com/api/trade/Order/GetTopLeaseOutOrderList?TemplateId={GoodsId}"
+
+        # 初始 Excel 生成
+        if not os.path.exists(ExcelFileName):
+            createExcel(ExcelFileName)
+
+        # 判断有无对应商品名称的 Sheet
+        createSheet(xlrd.open_workbook(ExcelFileName), GoodsId, ExcelFileName)
+
+        # 创建 JSON 文件
+        createJsonFile(GoodsId)
+
+        # 根据 JSON 文件向表格内的 Sheet 写入数据
+        writeDataToSheet(GoodsId, ExcelFileName, readDataFromJson(GoodsId))
