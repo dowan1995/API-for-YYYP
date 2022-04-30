4e0d938e8b67047da6a7adfa14478bc032f1d5cc
Dowan <46449568+dowan1995@users.noreply.github.com>
2022-04-30 13:09:32 +0800

    Create DuplicatedExcelToPath.py

---
+++ DuplicatedExcelToPath.py
@@ -0,0 +1,27 @@
+import os
+import time
+import zipfile
+
+import pandas as pd
+from goods import goods
+
+EXCELFILENAME = '租赁成交记录_' + time.strftime("%Y%m%d") + '.xls'
+PATH = '去重_' + time.strftime("%Y%m%d")
+
+
+def duplicateExcelAndSaveToPath():
+    if not os.path.exists(PATH):
+        os.mkdir(PATH)
+    z = zipfile.ZipFile(f'{time.strftime("%Y%m%d")}.zip', 'w')
+    for GoodsId in goods:
+        pd.read_excel(EXCELFILENAME, sheet_name=goods[GoodsId]).drop_duplicates(
+            subset=['租赁价格', '租赁时长', '租赁类型', '押金', '成交时间'], keep='last', inplace=False).to_excel(
+            f'{PATH}/{goods[GoodsId]}_租赁成交记录_' + time.strftime("%Y%m%d") + '_去重.xls',
+            sheet_name=goods[GoodsId])
+        z.write(f'{PATH}/{goods[GoodsId]}_租赁成交记录_' + time.strftime("%Y%m%d") + '_去重.xls')
+    z.write(EXCELFILENAME)
+    z.close()
+
+
+if __name__ == '__main__':
+    duplicateExcelAndSaveToPath()
