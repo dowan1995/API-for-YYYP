b1b0783fcbfcc21f6eff61fbdb85ffa6d89e1cea
Dowan <46449568+dowan1995@users.noreply.github.com>
2022-05-05 10:33:11 +0800

    Update DuplicatedExcelToPath.py

--- DuplicatedExcelToPath.py
+++ DuplicatedExcelToPath.py
@@ -1,7 +1,7 @@
 import os
 import time
+import shutil
 import zipfile
-
 import pandas as pd
 from goods import goods
 
@@ -21,6 +21,8 @@ def duplicateExcelAndSaveToPath():
         z.write(f'{PATH}/{goods[GoodsId]}_租赁成交记录_' + time.strftime("%Y%m%d") + '_去重.xls')
     z.write(EXCELFILENAME)
     z.close()
+    shutil.rmtree(PATH)
+    os.remove(EXCELFILENAME)
 
 
 if __name__ == '__main__':
